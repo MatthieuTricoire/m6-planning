48b2ae4dc8793f9e157b014de51a498ef16cc8e3
MatthieuTricoire <matthieutricoire@gmail.com>
2023-01-31 19:49:28 +0000

    Rajout du composant header

---
+++ src/components/Header.js
@@ -0,0 +1,9 @@
+import logo from "../assets/img/M6_logo.png";
+
+const Header = () => (
+  <header className="container row">
+    <img src={logo} alt="" />
+  </header>
+);
+
+export default Header;
